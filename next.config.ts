aadc4287f01c8452fc9475507442bb9b993aa455
1comce <mainvox9423@gmail.com>
2025-03-13 12:33:42 +0700

    revert tranpile

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: [
-    "next/dist/compiled/@next/react-dev-overlay",
-    "tailwind-merge",
-  ],
   images: {
     domains: ["image.tmdb.org"],
   },
