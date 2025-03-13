c3f5e209912b13251c0820ceee03395c7c592bea
1comce <mainvox9423@gmail.com>
2025-03-13 12:29:20 +0700

    test transpile for compatibility ios 12

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: [
+    "next/dist/compiled/@next/react-dev-overlay",
+    "tailwind-merge",
+  ],
   images: {
     domains: ["image.tmdb.org"],
   },
